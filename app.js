edf1bc02d8c48cc4768b1684669dade80361d277
Adkirf <adkirfuglyape@gmail.com>
2023-02-09 12:47:24 +0100

    add touch: home content first try

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const leftSwitch = document.querySelectorAll(".switch-btn")[1];
 const doneItems = document.querySelectorAll(".done-item");
 
 function PageTransitions() {
+  //Home-Touch
+  document.querySelector(".left-home").ontouchstart = 
+	() => document.querySelector(".flip-box").classList.toggle("touchbtn");
+  
   //Button click actvie class
   for (let i = 0; i < sectBtns.length; i++) {
     sectBtns[i].addEventListener("click", function () {
@@ -28,27 +32,27 @@ function PageTransitions() {
 function nextSwitchPossible(switchForwards) {
   for (let i = 0; i < doneItems.length; i++) {
     if (switchForwards) {
-      doneItems[i].classList.remove("left");
-      doneItems[i].classList.replace("middle", "left");
-      if (doneItems[i].classList.replace("right", "middle")) {
+      doneItems[i].classList.remove("last");
+      doneItems[i].classList.replace("current", "last");
+      if (doneItems[i].classList.replace("next", "current")) {
         if (i === doneItems.length - 1) {
           rightSwitch.classList.add("list-end");
         } else {
-          doneItems[i + 1].classList.add("right");
+          doneItems[i + 1].classList.add("next");
           leftSwitch.classList.remove("list-end");
         }
         break;
       }
     } else {
-      doneItems[doneItems.length - 1 - i].classList.remove("right");
-      doneItems[doneItems.length - 1 - i].classList.replace("middle", "right");
+      doneItems[doneItems.length - 1 - i].classList.remove("next");
+      doneItems[doneItems.length - 1 - i].classList.replace("current", "next");
       if (
-        doneItems[doneItems.length - 1 - i].classList.replace("left", "middle")
+        doneItems[doneItems.length - 1 - i].classList.replace("last", "current")
       ) {
         if (doneItems.length - 1 - i === 0) {
           leftSwitch.classList.add("list-end");
         } else {
-          doneItems[doneItems.length - i - 2].classList.add("left");
+          doneItems[doneItems.length - i - 2].classList.add("last");
           rightSwitch.classList.remove("list-end");
         }
         break;
